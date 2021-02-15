782cf3527ad5ae3ed78d23024b689ded000c1033
Daniel Commins <dencee@users.noreply.github.com>
2021-02-15 11:50:27 -0800

    Modularized so more criteria can be added

--- Level3-Module3/src/_03_Intro_To_Regular_Expressions/_02_PasswordChecker.java
+++ Level3-Module3/src/_03_Intro_To_Regular_Expressions/_02_PasswordChecker.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import javax.swing.BorderFactory;
@@ -12,9 +13,8 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 
 /*
  * Use regex to check if a password meets the minimum criteria
@@ -25,56 +25,66 @@ public class _02_PasswordChecker implements KeyListener {
      * 1. Check a minimum of 8 characters
      */
     static final String regexCriteria1 = "";
-    
+
     /*
      * 2. Check there is at least 1 number, i.e. a digit 0-9
      */
     static final String regexCriteria2 = "";
-    
+
     /*
      * 3. Check there is at least 1 capital letter
      */
     static final String regexCriteria3 = "";
-    
+
     /*
      * 4. Check there is at least 1 special character, where special
      * characters are one of the following, @#$%^&-+=()
      */
     static final String regexCriteria4 = "";
-
-    static final String INCORRECT = " X - ";
-    static final String CORRECT = " + - ";
+    
+    /*
+     * *BONUS* Add more password criteria. Some suggestions are:
+     *  - check for no whitespace characters
+     *  - check for lower case letters
+     *  - check for maximum of 20 characters
+     *  
+     *  Below is a more rigorous regex password checker. Can you figure out
+     *  what it's checking?
+     *  \A(?=\w{6,10}\z)(?=[^a-z]*[a-z])(?=(?:[^A-Z]*[A-Z]){3})\D*\d.*\z
+     *  
+     *  
+     *  http://www.rexegg.com/regex-lookarounds.html#password
+     */
+    
+    static final String INCORRECT = " X ";
+    static final String CORRECT = " + ";
+    static final boolean USE_FIND = true;
+    static final boolean USE_MATCH = false;
     static final String CRITERIA1 = "A minimum of 8 characters";
     static final String CRITERIA2 = "Must contain at least 1 number";
     static final String CRITERIA3 = "Must contain at least 1 capital letter";
     static final String CRITERIA4 = "Must contain at least 1 special character";
     static Font textFont = new Font( "Arial", Font.PLAIN, 18);
-    JLabel criteriaMinimumLength;
-    JLabel criteriaNumber;
-    JLabel criteriaCapitalLetter;
-    JLabel criteriaSpecialCharacter;
-    JTextField passwordField;
-    String password = "";
-    String asterixPassword = "";
+    JPasswordField passwordField;
+    ArrayList<Criteria> criterias;
 
     public _02_PasswordChecker() {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         JLabel instructions = new JLabel("Please create a password that meets the following criteria:");
-        passwordField = new JTextField(20);
-        criteriaMinimumLength = new JLabel(INCORRECT + CRITERIA1);
-        criteriaNumber = new JLabel(INCORRECT + CRITERIA2);
-        criteriaCapitalLetter = new JLabel(INCORRECT + CRITERIA3);
-        criteriaSpecialCharacter = new JLabel(INCORRECT + CRITERIA4);
-
-        criteriaMinimumLength.setForeground(Color.RED);
-        criteriaNumber.setForeground(Color.RED);
-        criteriaCapitalLetter.setForeground(Color.RED);
-        criteriaSpecialCharacter.setForeground(Color.RED);
-        
+        passwordField = new JPasswordField(20);
         instructions.setFont(textFont);
         passwordField.setFont(textFont);
-
+        
+        /*
+         * Add new criteria below!
+         */
+        criterias = new ArrayList<Criteria>();
+        criterias.add(new Criteria(CRITERIA1, regexCriteria1, USE_MATCH));
+        criterias.add(new Criteria(CRITERIA2, regexCriteria2, USE_FIND));
+        criterias.add(new Criteria(CRITERIA3, regexCriteria3, USE_FIND));
+        criterias.add(new Criteria(CRITERIA4, regexCriteria4, USE_FIND));
+        
         panel.setLayout( new BoxLayout(panel, BoxLayout.Y_AXIS) );
         panel.setBorder(BorderFactory.createCompoundBorder( passwordField.getBorder(), 
                 BorderFactory.createEmptyBorder(0, 5, 0, 5)));
@@ -84,10 +94,9 @@ public class _02_PasswordChecker implements KeyListener {
         
         // Keep the order of add() 
         panel.add(instructions);
-        panel.add(criteriaMinimumLength);
-        panel.add(criteriaNumber);
-        panel.add(criteriaCapitalLetter);
-        panel.add(criteriaSpecialCharacter);
+        for( Criteria criteria : criterias ) {
+            panel.add(criteria.label);
+        }
         panel.add(Box.createRigidArea(new Dimension(0, 25)));
         panel.add(passwordField);
         panel.add(Box.createRigidArea(new Dimension(0, 25)));
@@ -99,98 +108,70 @@ public class _02_PasswordChecker implements KeyListener {
         frame.pack();
     }
     
-    public boolean checkPasswordCriteria() {
-        boolean goodPassword = true;
-        
-        // Minimum length. Use matches() to check entire password string
-        if( !regexCriteria1.isEmpty() &&
-            Pattern.compile(regexCriteria1).matcher(this.password).matches() )
-        {
-            criteriaMinimumLength.setText(CORRECT + CRITERIA1);
-            criteriaMinimumLength.setForeground(Color.GREEN);
-        } else {
-            criteriaMinimumLength.setText(INCORRECT + CRITERIA1);
-            criteriaMinimumLength.setForeground(Color.RED);
-            goodPassword = false;
-        }
-        
-        // At least 1 digit. Use find() for substring within password string
-        if( !regexCriteria2.isEmpty() &&
-            Pattern.compile(regexCriteria2).matcher(this.password).find() )
-        {
-            criteriaNumber.setText(CORRECT + CRITERIA2);
-            criteriaNumber.setForeground(Color.GREEN);
-        } else {
-            criteriaNumber.setText(INCORRECT + CRITERIA2);
-            criteriaNumber.setForeground(Color.RED);
-            goodPassword = false;
-        }
-        
-        // At least 1 capital letter. Use find() for substring within password string
-        if( !regexCriteria3.isEmpty() &&
-            Pattern.compile(regexCriteria3).matcher(this.password).find() )
-        {
-            criteriaCapitalLetter.setText(CORRECT + CRITERIA3);
-            criteriaCapitalLetter.setForeground(Color.GREEN);
-        } else {
-            criteriaCapitalLetter.setText(INCORRECT + CRITERIA3);
-            criteriaCapitalLetter.setForeground(Color.RED);
-            goodPassword = false;
-        }
-        
-        // At least 1 special character. Use find() for substring within password string
-        if( !regexCriteria4.isEmpty() &&
-            Pattern.compile( regexCriteria4).matcher(this.password).find() )
-        {
-            criteriaSpecialCharacter.setText(CORRECT + CRITERIA4);
-            criteriaSpecialCharacter.setForeground(Color.GREEN);
-        } else {
-            criteriaSpecialCharacter.setText(INCORRECT + CRITERIA4);
-            criteriaSpecialCharacter.setForeground(Color.RED);
-            goodPassword = false;
-        }
-        
-        return goodPassword;
-    }
-
     @Override
-    public void keyPressed(KeyEvent arg0) {
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
         
     }
 
     @Override
-    public void keyReleased(KeyEvent arg0) {
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
         
     }
 
     @Override
-    public void keyTyped(KeyEvent arg0) {
-        int keyCode = (int)arg0.getKeyChar();
+    public void keyTyped(KeyEvent e) {
+        String password = new String( passwordField.getPassword() ) + e.getKeyChar();
         
-        if (keyCode == KeyEvent.VK_ENTER) {
-            if( checkPasswordCriteria() ) {
-                JOptionPane.showMessageDialog(null, "Successful password!");
+        for( Criteria criteria : criterias ) {
+            criteria.match(password);
+        }
+    }
+    
+    class Criteria {
+        String criteriaDescription;
+        String regex;
+        boolean useFindToValidate;
+        JLabel label;
+        
+        Criteria(String description, String regex, boolean useFind) {
+            this.criteriaDescription = description;
+            this.regex = regex;
+            this.useFindToValidate = useFind;
+            
+            this.label = new JLabel(INCORRECT + this.criteriaDescription);
+            this.label.setForeground(Color.RED);
+        }
+        
+        // Match inserts implicit starting and ending anchors to the regex
+        public boolean match(String password) {
+            return validate(password, this.useFindToValidate);
+        }
+
+        // Call match() instead
+        private boolean validate(String password, boolean useFind) {
+            boolean validPassword = true;
+            boolean regexSuccess;
+            
+            // Match inserts implicit starting and ending anchors to the regex
+            // Use find if that's not desired
+            if(useFind) {
+                regexSuccess = Pattern.compile(this.regex).matcher(password).find();
             } else {
-                JOptionPane.showMessageDialog(null, "ERROR: Password doesn't meet password criteria.");
-            }
-        } else if( keyCode == KeyEvent.VK_BACK_SPACE ) {
-            if( this.password.length() > 0 ) {
-                this.password = this.password.substring(0, this.password.length() - 1);
+                regexSuccess = Pattern.compile(this.regex).matcher(password).matches();
             }
             
-            if( this.asterixPassword.length() > 0 ) {
-                this.asterixPassword = this.asterixPassword.substring(0, this.asterixPassword.length() - 1);
+            if( !this.regex.isEmpty() && regexSuccess ) {
+                this.label.setText(CORRECT + this.criteriaDescription);
+                this.label.setForeground(Color.GREEN);
+            } else {
+                this.label.setText(INCORRECT + this.criteriaDescription);
+                this.label.setForeground(Color.RED);
+                validPassword = false;
             }
             
-            checkPasswordCriteria();
-        } else if( keyCode == 127 ){
-            // Delete key not allowed, only use backspace key for coding simplicity
-            passwordField.setText( this.asterixPassword );
-        } else {
-            passwordField.setText( this.asterixPassword );
-            this.password += (char)keyCode;
-            this.asterixPassword += "*";
-            checkPasswordCriteria();
+            return validPassword;
         }
     }
 }
